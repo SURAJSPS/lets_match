d897d509d50c66292bf9b673fe386c29c012e4de
surajsingh <surajsps@icloud.com>
2021-07-26 15:31:58 +0530

    work done

--- lib/bottom_tab/home.dart
+++ lib/bottom_tab/home.dart
@@ -1,7 +1,7 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:lets_match/util/user_match_util.dart';
-import 'package:lets_match/widget/user_match_card.dart';
+import 'package:lets_match/model/user_match_model.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,6 +9,165 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
+
+final List<UserMatchModel> imgList = [
+  UserMatchModel(
+      image:
+          'https://secure.gravatar.com/avatar/ea4a232b2a8c2b116ef27574d8c0abb7?s=400&d=mm&r=g',
+      location: 'Usa',
+      name: 'Rina',
+      religion: 'Hindu'),
+  UserMatchModel(
+      image:
+          'https://secure.gravatar.com/avatar/ea4a232b2a8c2b116ef27574d8c0abb7?s=400&d=mm&r=g',
+      location: 'Usa',
+      name: 'Rina',
+      religion: 'Hindu'),
+  UserMatchModel(
+      image:
+          'https://secure.gravatar.com/avatar/ea4a232b2a8c2b116ef27574d8c0abb7?s=400&d=mm&r=g',
+      location: 'Usa',
+      name: 'Rina',
+      religion: 'Hindu'),
+  UserMatchModel(
+      image:
+          'https://secure.gravatar.com/avatar/ea4a232b2a8c2b116ef27574d8c0abb7?s=400&d=mm&r=g',
+      location: 'Usa',
+      name: 'Rina',
+      religion: 'Hindu'),
+  UserMatchModel(
+      image:
+          'https://secure.gravatar.com/avatar/ea4a232b2a8c2b116ef27574d8c0abb7?s=400&d=mm&r=g',
+      location: 'Usa',
+      name: 'Rina',
+      religion: 'Hindu'),
+];
+final List<Widget> imageSliders = imgList
+    .map(
+      (item) => Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50), color: Colors.white),
+            child: Padding(
+              padding: const EdgeInsets.all(3),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(50),
+                child: Image.network(
+                  "${item.image}",
+                  height: 190,
+                  width: 190,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            "${item.name}",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            "${item.location}",
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.grey),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          RichText(
+            text: TextSpan(
+              text: 'Religion: ',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey),
+              children: <TextSpan>[
+                TextSpan(
+                  text: "${item.religion}",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold, color: Colors.pink),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(15)),
+                child: IconButton(
+                  icon: Icon(
+                    CupertinoIcons.heart,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(15)),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.message,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: 40,
+                width: 40,
+                decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(15)),
+                child: IconButton(
+                  icon: Icon(
+                    CupertinoIcons.down_arrow,
+                    color: Colors.white,
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(
+                width: 5,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30,
+          )
+        ],
+      ),
+    )
+    .toList();
 
 class _HomeState extends State<Home> {
   @override
@@ -84,17 +243,17 @@ Widget _getMatchUser(context) {
         height: 80,
       ),
       Container(
-        height: MediaQuery.of(context).size.height / 2.54,
-        child: ListView.builder(
-          addSemanticIndexes: true,
-          itemBuilder: (context, index) {
-            return UserMatchCard(
-              userMatchModel: UserMatchUtil.list[index],
-            );
-          },
-          itemCount: UserMatchUtil.list.length,
+        // color: Colors.black,
+        height: MediaQuery.of(context).size.height / 1.71,
+        child: CarouselSlider(
+          options: CarouselOptions(
+            aspectRatio: 2.0,
+            enlargeCenterPage: true,
+            scrollDirection: Axis.vertical,
+          ),
+          items: imageSliders,
         ),
-      ),
+      )
     ],
   );
 }
